3e8ba74d2463ec4954611064c1b9c21c268667d9
Craig <142868948+CraigTheSpaceBum@users.noreply.github.com>
2025-09-28 20:34:11 -0400

    Initial Vite+React Pixel Pets app with GitHub Pages deploy setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TamagotchiApp from './TamagotchiApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TamagotchiApp />
+  </React.StrictMode>
+);
