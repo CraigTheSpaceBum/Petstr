25001df08efc4fa9f77e2ea383de9e8856d027c7
Craig <142868948+CraigTheSpaceBum@users.noreply.github.com>
2025-10-03 23:16:38 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TamagotchiApp from './TamagotchiApp';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TamagotchiApp />
-  </React.StrictMode>
-);
